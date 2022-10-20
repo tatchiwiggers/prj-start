3d50a4de813923690d2b22dad8f60bf90379a8ed
tatchiwiggers <tatisabato@hotmail.com>
2022-10-20 16:45:56 -0300

    adds navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingItemComponent } from './shopping-list/shopping-item/shopping-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { ShoppingItemComponent } from './shopping-list/shopping-item/shopping-it
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingItemComponent
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
